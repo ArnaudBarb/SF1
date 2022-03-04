7ff8980716b9bee3b01f530a5cfc72f8f2f503ef
Arnaud Barberon <barberon_a@hotmail.fr>
2022-03-04 12:44:56 +0100

    losdfj

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -11,10 +11,18 @@ class PageController extends AbstractController
     #[Route('/page', name: 'app_page')]
     public function index(): Response
     {
-        return $this->file('fichier.txt');
+        return $this->render('base.html.twig');
+        // return $this->file('fichier.txt');va télécharger le fichier.txt dans le dossier téléchargement
         // return $this->json([
         //     'message' => 'Welcome to your new controller!',
         //     'path' => 'src/Controller/PageController.php',
-        // ]);
+        // ]);//retourne le contenu en format json
+    }
+    #[Route('/machin', name:'app_machin')]
+    public function machin(): Response
+    {
+        throw $this->createNotFoundException('Welcome to the void');
     }
 }
+
+
