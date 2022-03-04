37a95ed8bbf5a5755d87940ab3c7bf2a1432496c
Arnaud Barberon <barberon_a@hotmail.fr>
2022-03-04 12:23:40 +0100

    installation de twig

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -11,9 +11,10 @@ class PageController extends AbstractController
     #[Route('/page', name: 'app_page')]
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PageController.php',
-        ]);
+        return $this->file('fichier.txt');
+        // return $this->json([
+        //     'message' => 'Welcome to your new controller!',
+        //     'path' => 'src/Controller/PageController.php',
+        // ]);
     }
 }
